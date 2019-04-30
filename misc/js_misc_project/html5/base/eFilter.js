4eccdafc30af91b8c5adae53a796d5b817838d7b
csy9730 <csy9730@users.noreply.github.com>
2019-05-01 01:29:01 +0800

    add js on 20190501

--- misc/js_misc_project/html5/base/eFilter.js
+++ misc/js_misc_project/html5/base/eFilter.js
@@ -1,3 +1,4 @@
+// filter,map,some,lastIndexOf(), indexOf():等函数demos
 var ages = [32, 33, 16, 40];
 
 function checkAdult(age) {
@@ -7,3 +8,55 @@ function checkAdult(age) {
 function myFunction() {
     document.getElementById("demo").innerHTML = ages.filter(checkAdult);
 }
+
+// forEach()会改变原始的数组的值，而map()会返回一个全新的数组
+//  将所有的数组元素转换为大写：
+var strings = ["hello", "Array", "WORLD"];
+function makeUpperCase(v)
+{
+    return v.toUpperCase();
+}
+var uppers = strings.map(makeUpperCase);
+
+//
+//检查是否有数组元素大于等于10：
+
+function isBigEnough(element, index, array) {
+    return (element >= 10);
+}
+var passed = [2, 5, 8, 1, 4].some(isBigEnough);
+// passed is false
+passed = [12, 5, 8, 1, 4].some(isBigEnough);
+// passed is true
+//结果：
+//[2, 5, 8, 1, 4].some(isBigEnough) ： false 
+//[12, 5, 8, 1, 4].some(isBigEnough) ： true 
+
+
+//测试是否所有数组元素都大于等于10：
+
+function isBigEnough(element, index, array) {
+    return (element >= 10);
+}
+var passed = [12, 5, 8, 130, 44].every(isBigEnough);
+// passed is false
+passed = [12, 54, 18, 130, 44].every(isBigEnough);
+// passed is true
+//结果：
+//[12, 5, 8, 130, 44].every(isBigEnough) 返回 ： false 
+//[12, 54, 18, 130, 44].every(isBigEnough) 返回 ： true 
+
+//打印数组内容：
+
+function printElt(element, index, array) {
+    document.writeln("[" + index + "] is " + element + "<br />");
+}
+[2, 5, 9].forEach(printElt);
+// Prints:
+// [0] is 2
+// [1] is 5
+// [2] is 9
+//结果：
+//[0] is 2
+//[1] is 5
+//[2] is 9
---
+++ misc/js_misc_project/html5/base/eInt2string.js
@@ -0,0 +1,9 @@
+var   x=100   
+a   =   x.toString()   
+var   x2=100;   
+a2   =   x2   +"";     //JS会自动隐性转换  
+
+parseInt("abc") // Returns NaN. 
+parseInt("12abc") // Returns 12. 
+parseInt("12") //Return 12.  
+parseFloat("12.345") //Return 12.  
