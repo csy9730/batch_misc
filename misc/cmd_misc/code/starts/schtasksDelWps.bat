b6fdcc0a0fd0ad2a4b2a7dc48fb92cf0a020187b
unknown <gd_csy@163.com>
2019-05-30 18:37:38 +0800

    add regedit.shellopenmenu

--- misc/cmd_misc/code/starts/schtasksDelWps.bat
+++ misc/cmd_misc/code/starts/schtasksDelWps.bat
@@ -1,2 +1,2 @@
-schtasks /delete /tn /f WpsUpdateTask_admin
-schtasks /delete /tn /f WpsExternal_admin_20190428132612
+schtasks /delete  /f /tn WpsUpdateTask_admin
+schtasks /delete /f /tn WpsExternal_admin_20190428132612
